6b61ed5b611c2fbc9e7c7d7347b2d1d57858d591
matuyama <143941798+matuyama@users.noreply.github.com>
2023-09-11 09:28:14 +0000

    [add]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  
+  resources :post_images, only: [:mew, :index, :show]
+  
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', as: 'about'
